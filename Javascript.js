de2acbfaf5ab9b3034c1cb1eaa65b96fdc2fae64
samarth5993 <125127551+samarth5993@users.noreply.github.com>
2023-03-10 09:38:15 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".button-container-1").on("click",function(){
+        $('body').toggleClass("body-color");
+        $(".button-container-1").toggleClass("button-container");
+        $("h1").toggleClass("text");
+        $(".button-1").toggleClass("button");
+    })
+}); 
